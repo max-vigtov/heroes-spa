7b457be3ff7443e46c03c968c5a6a9511c24a2f5
Max Vigueras <mvt.2000@hotmail.com>
2025-04-19 00:55:50 -0600

    Heroes: Hero List & Hero Card added.

--- src/heroes/pages/DcPage.tsx
+++ src/heroes/pages/DcPage.tsx
@@ -1,5 +1,11 @@
+import { HeroeList } from "../components"
+
 export const DcPage = () => {
   return (
-	<h1>DcPage</h1>
+    <>
+      <h1>Dc Page</h1>
+      <hr />
+      <HeroeList publisher="DC Comics" />
+    </>
   )
 }
--- src/heroes/pages/MarvelPage.tsx
+++ src/heroes/pages/MarvelPage.tsx
@@ -1,5 +1,11 @@
+import { HeroeList } from "../components"
+
 export const MarvelPage = () => {
   return (
-	<h1>MarvelPage</h1>
+    <>
+      <h1>MarvelPage</h1>
+      <hr />
+      <HeroeList publisher="Marvel Comics" />
+    </>
   )
 }
