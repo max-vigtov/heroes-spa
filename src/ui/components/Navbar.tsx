c19d134496a4d8ca1d2620da7531b578441561a2
Max Vigueras <mvt.2000@hotmail.com>
2025-04-18 19:28:03 -0600

    Login: Login routing implemented

--- src/ui/components/Navbar.tsx
+++ src/ui/components/Navbar.tsx
@@ -1,6 +1,13 @@
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 
 export const Navbar = () => {
+
+    const navigate = useNavigate();
+
+    const onLogout = () => {
+       navigate('/login', {replace: true})
+    }
+
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
             <div className="container-fluid">
@@ -34,7 +41,10 @@ export const Navbar = () => {
                                 Max Vigueras
                             </span>
 
-                            <button className="nav-item nav-link btn">
+                            <button 
+                                className="nav-item nav-link btn"
+                                onClick={onLogout}
+                            >
                                 Logout 
                             </button>
                         </ul>
